823f12b905ba23d1f659b44ccbfa1d2e588f6033
tejasvisawant1998@gmail.com <tejasvisawant1998@gmail.com>
2023-11-06 21:19:39 +0530

    first commit

---
+++ src/Components/GitHub/GitHub.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GitHub = () => {
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>GitHub</div>
+  )
+}
+
+export default GitHub
